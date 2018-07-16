73d2876e983874d398e5a4ba38926b4acbdd97f2
Aaron Pruzaniec <aaronp@gwynniebee.com>
2018-07-16 02:17:47 -0400

    adapted more gwynnie bee v4 pages

---
+++ gwynniebee/pages/tracy-reese-lookbook/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<html>
+<?php include("../../common/header.php"); ?>
+  <body>
+<?php include("html/html.php"); ?>
+  </body>
+</html>
